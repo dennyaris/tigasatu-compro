740aa2cd42bf028a56dedbc89d2036ed95f530b5
Putra Fajar H <putrafajarh@gmail.com>
2024-01-30 16:15:51 +0700

    fix: exclude draft content from sitemap

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -2,7 +2,7 @@ import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
 import { serverQueryContent } from '#content/server'
 
 export default defineSitemapEventHandler(async (e) => {
-  const contentList = (await serverQueryContent(e).find()) as ParsedContent[]
+  const contentList = (await serverQueryContent(e).where({ _draft: false }).find()) as ParsedContent[]
   return contentList
     .filter(c => c._path!.startsWith('/career'))
     .map((c) => {
